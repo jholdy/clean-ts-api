525ee1b73bb5283321f97208d3e39d0a9e7be8a0
Jholdy Damasceno <jholdy@Jholdys-MacBook-Pro.local>
2020-09-29 04:41:53 -0300

    chore: fixbug config

--- src/main/config/app.ts
+++ src/main/config/app.ts
@@ -1,7 +1,9 @@
 import express from 'express'
 import setupMiddlewares from './middlewares'
 import setupRoutes from './routes'
+import swagger from './swagger'
 
 export const app = express()
+swagger(app)
 setupMiddlewares(app)
 setupRoutes(app)
