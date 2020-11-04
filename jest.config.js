6e7e9ec4a058260446b943cd8054ef06af447e63
Jholdy Damasceno <jholdydamasceno@gmail.com>
2020-11-04 09:06:03 +0000

    feat: correcao tests, docker, swagger e etc

--- jest.config.js
+++ jest.config.js
@@ -4,7 +4,6 @@ module.exports = {
   collectCoverageFrom: ["<rootDir>/src/**/*.ts", "<rootDir>/__TESTS__/**/*.ts", "!<rootDir>/src/main/**"],
   coverageDirectory: "coverage",
   testEnvironment: "node",
-  preset: "@shelf/jest-mongodb",
   transform: {
     ".+\\.ts$": "ts-jest"
   },
